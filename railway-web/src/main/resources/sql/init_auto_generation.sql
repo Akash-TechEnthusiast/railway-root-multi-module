713cc869c1869f38fd1cbc217636afadc4d1a058
akash922.g@gmail.com <akash922.g@gmail.com>
2025-12-23 15:27:10 +0530

    sql file chanegs

--- railway-web/src/main/resources/sql/init_auto_generation.sql
+++ railway-web/src/main/resources/sql/init_auto_generation.sql
@@ -1,76 +1,23 @@
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('1', '1', 'passenger_entity', '', '');
 
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('1', '1', 'passenger_entity', '', ''
-    );
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('2', '1', 'address_entity', '', '');
 
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('2', '1', 'address_entity', '', ''
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('3', '1', 'user_entity', '', '');
 
-    );
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('4', '1', 'train_entity', '', '');
 
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('3', '1', 'user_entity', '', ''
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('5', '1', 'passenger_pnr_number', 'IND/RAIL/PNR/', '2024/');
 
-    );
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('6', '1', 'train_number', 'IND/RAIL/TRN/', '2024/');
 
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('4', '1', 'train_entity', '', ''
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('7', '1', 'user_number', 'IND/RAIL/USR/', '2024/');
 
-    );
-
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('5', '1', 'passenger_pnr_number', 'IND/RAIL/PNR/', '2024/'
-
-    );
-
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('6', '1', 'train_number', 'IND/RAIL/TRN/', '2024/'
-
-    );
-
-INSERT INTO `auto_code_generation`
-(`id`,
- `current_value`,
- `entity_name`,
- `prefix`,
- `year`)
-VALUES
-    ('7', '1', 'user_number', 'IND/RAIL/USR/', '2024/'
-
-    );
+INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
+VALUES ('8', '1', 'user_numberR', 'IND/RAIL/USER/', '2024/');
