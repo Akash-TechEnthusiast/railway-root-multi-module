7664ec6f753136baa30f2d414fc6dcf65f3da3c0
akash922.g@gmail.com <akash922.g@gmail.com>
2025-12-26 12:58:29 +0530

    adding sql file chnages

--- railway-web/src/main/resources/sql/init_auto_generation.sql
+++ railway-web/src/main/resources/sql/init_auto_generation.sql
@@ -14,19 +14,30 @@ INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`,
 VALUES ('5', '1', 'passenger_pnr_number', 'IND/RAIL/PNR/', '2024/');
 
 INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
-VALUES ('6', '1', 'train_number', 'IND/RAIL/TRN/', '2024/');
+VALUES ('6', '9', 'train_number', 'IND/RAIL/TRN/', '2024/');
 
 INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
-VALUES ('7', '1', 'user_number', 'IND/RAIL/USR/', '2024/');
+VALUES ('7', '11', 'user_number', 'IND/RAIL/USR/', '2024/');
 
 INSERT IGNORE INTO `auto_code_generation` (`id`, `current_value`, `entity_name`, `prefix`, `year`)
 VALUES ('8', '1', 'user_numberR', 'IND/RAIL/USER/', '2024/');
 
-INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('1', 'kk', 'IND/RAIL/TRN/2024/00006');
-INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('2', 'k2', 'IND/RAIL/TRN/2024/00007');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('1', 'kk', 'IND/RAIL/TRN/2024/00001');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('2', 'k2', 'IND/RAIL/TRN/2024/00002');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('3', 'kk', 'IND/RAIL/TRN/2024/00003');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('4', 'k2', 'IND/RAIL/TRN/2024/00004');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('5', 'kk', 'IND/RAIL/TRN/2024/00005');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('6', 'k2', 'IND/RAIL/TRN/2024/00006');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('7', 'kk', 'IND/RAIL/TRN/2024/00007');
+INSERT IGNORE INTO `train` (`id`, `train_name`, `train_number`) VALUES ('8', 'k2', 'IND/RAIL/TRN/2024/00008');
 
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00001', 'password123', 'IND/RAIL/USR/2024/00001', 'akashuser');
 INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00002', 'password123', 'IND/RAIL/USR/2024/00002', 'akashuser');
 INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00003', 'password123', 'IND/RAIL/USR/2024/00003', 'akashuser');
 INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00004', 'password123', 'IND/RAIL/USR/2024/00004', 'akashuser');
-INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00005', 'password123', 'IND/RAIL/USR/2024/00004', 'akashuser');
-INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00006', 'password123', 'IND/RAIL/USR/2024/00004', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00005', 'password123', 'IND/RAIL/USR/2024/00005', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00006', 'password123', 'IND/RAIL/USR/2024/00006', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00007', 'password123', 'IND/RAIL/USR/2024/00007', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00008', 'password123', 'IND/RAIL/USR/2024/00008', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00009', 'password123', 'IND/RAIL/USR/2024/00009', 'akashuser');
+INSERT IGNORE INTO `users` (`id`, `password`, `user_number`, `username`) VALUES ('00010', 'password123', 'IND/RAIL/USR/2024/00010', 'akashuser');
