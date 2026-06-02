61b4f8a5d2e14f23a13e75f2d0cf94b6991f00fe
akash922.g@gmail.com <akash922.g@gmail.com>
2026-06-02 11:08:00 +0530

    adding new changes

--- railway-web/src/main/java/com/india/railway/model/mysql/Student_Mysql.java
+++ railway-web/src/main/java/com/india/railway/model/mysql/Student_Mysql.java
@@ -22,8 +22,144 @@ public class Student_Mysql {
     private String dob;
     private boolean agreeTerms;
     private boolean notifications;
-
     private String country;
+    private String address;
+    private String bloodGroup;
+    private String religion;
+    private String castecategory;
+    private String pincode;
+    private String village;
+    private String state;
+    private String district;
+    private String mothername;
+    private String fathername;
+    private String aadharno;
+    private String panno;
+    private String schoolname;
+    private String profile;
+    private String tc;
+    private String sscMemo;
+
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getCastecategory() {
+        return castecategory;
+    }
+
+    public void setCastecategory(String castecategory) {
+        this.castecategory = castecategory;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getMothername() {
+        return mothername;
+    }
+
+    public void setMothername(String mothername) {
+        this.mothername = mothername;
+    }
+
+    public String getFathername() {
+        return fathername;
+    }
+
+    public void setFathername(String fathername) {
+        this.fathername = fathername;
+    }
+
+    public String getAadharno() {
+        return aadharno;
+    }
+
+    public void setAadharno(String aadharno) {
+        this.aadharno = aadharno;
+    }
+
+    public String getPanno() {
+        return panno;
+    }
+
+    public void setPanno(String panno) {
+        this.panno = panno;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getTc() {
+        return tc;
+    }
+
+    public void setTc(String tc) {
+        this.tc = tc;
+    }
+
+    public String getSscMemo() {
+        return sscMemo;
+    }
+
+    public void setSscMemo(String sscMemo) {
+        this.sscMemo = sscMemo;
+    }
 
     public Long getId() {
         return id;
@@ -114,6 +250,14 @@ public class Student_Mysql {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     @Override
